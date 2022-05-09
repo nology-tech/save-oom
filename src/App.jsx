3bdc62e81657cc9716e7c0f8f6696de0e9933737
Lyssa <ebonwolf00@gmail.com>
2022-05-09 17:26:11 +0100

    fixed where NavBarHeading is called

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import './styles/main.scss';
-import NavBarHeading from './components/navBarHeading/NavBarHeading';
 
 const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path='*' element={<Home />} />
-      </Routes>
-      <NavBarHeading />
-    </>
+    <Routes>
+      <Route path='*' element={<Home />} />
+    </Routes>
   );
 };
 
