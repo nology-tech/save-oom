a233af2415eaf1ea896732a3561bff84bed0e20c
Patricia Edwards <patriciaje16@gmail.com>
2022-05-10 15:32:37 +0100

    created story container

---
+++ src/components/StoryIntro/StoryIntro.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./StoryIntro.scss"
+
+const StoryIntro = () => {
+  return (
+    <div className='intro'>
+      <p className='intro__paragraph' >Once upon a time, there was an Alien, whose name was Oom. Oom lived in Space, on planet XXXX.</p>
+
+      <p className='intro__paragraph'>Oom was very excited because when aliens turn 5, they get to fly spaceships for the very first time.  </p>
+
+      <p className='intro__paragraph'>Can you see Oom waving? Say Hello to Oom!! </p>
+
+
+    </div>
+  )
+}
+
+export default StoryIntro
