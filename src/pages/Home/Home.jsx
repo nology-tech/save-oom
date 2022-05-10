d8cf487ee0273c23aae38753adc4834d1abfc4de
Lyssa <99736403+Ebonwulf@users.noreply.github.com>
2022-05-10 15:47:45 +0100

    phonics stage heading and image created and styling done (#5)
    * phonics stage heading and image created and styling done
    
    * fixed where NavBarHeading is called
    
    * colour variables added and hopefully the npm issue solved
    
    * hex colour put back in after variable broke it
    
    * snapshot issues resolved and scss px converted to rem

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,15 @@
-import Layout from "../../components/Layout/Layout";
+import Layout from '../../components/Layout/Layout';
+import NavBarHeading from '../../components/navBarHeading/NavBarHeading';
 
 const Home = () => {
   return (
-    <Layout>
-      <h1>Save Oom!!</h1>
-      <p>
-        Let&apos;s save the little critter!
-      </p>
-    </Layout>
+    <>
+      <Layout>
+        <h1>Save Oom!!</h1>
+        <p>Let&apos;s save the little critter!</p>
+      </Layout>
+      <NavBarHeading />
+    </>
   );
 };
 
