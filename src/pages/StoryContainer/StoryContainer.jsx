f483160c5961a2aa1780d899e17572e0992ac78a
patriciaje16 <100690509+patriciaje16@users.noreply.github.com>
2022-05-10 15:39:07 +0100

    created story container (#8)

---
+++ src/pages/StoryContainer/StoryContainer.jsx
@@ -0,0 +1,12 @@
+import "./StoryContainer.scss"
+import React from 'react'
+
+const StoryContainer = () => {
+  return (
+    <div className="story__container">
+
+    </div>
+  )
+}
+
+export default StoryContainer
