8e76d5f30215c2d289f55fbd529c6a1daadd46b8
Emma Smith <smileandsparkle@hotmail.com>
2022-05-11 15:30:50 +0100

    rendered story container and nav bar to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,17 @@
-import Layout from '../../components/Layout/Layout';
+// import Layout from '../../components/Layout/Layout';
 import NavBar from '../../components/navBar/NavBar';
+import StoryContainer from '../StoryContainer/StoryContainer';
+import "./Home.scss"
 const Home = () => {
   return (
-    <>
-      <Layout>
+    <div className='home'>
+      {/* <Layout>
         <h1>Save Oom!!</h1>
         <p>Let&apos;s save the little critter!</p>
-      </Layout>
+      </Layout> */}
       <NavBar />
-    </>
+      <StoryContainer />
+    </div>
   );
 };
 
--- src/pages/StoryContainer/StoryContainer.jsx
+++ src/pages/StoryContainer/StoryContainer.jsx
@@ -1,11 +1,13 @@
 import "./StoryContainer.scss"
 import React from 'react'
 import StoryIntro from "../../components/StoryIntro/StoryIntro"
+import Button from "../../components/Button/Button"
 
 const StoryContainer = () => {
   return (
     <div className="story__container">
       <StoryIntro />
+      <Button buttonStyle={"button-primary"} buttonText={"NEXT"}/>
 
     </div>
   )
