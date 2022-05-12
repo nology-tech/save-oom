35d44c8a3a222de7a9f28480688c3ca258be4d40
n-a-z-a <nadesper@gmail.com>
2022-05-12 15:45:02 +0100

    completed and tested avatar heading

--- src/components/AvatarHeading/AvatarHeading.test.jsx
+++ src/components/AvatarHeading/AvatarHeading.test.jsx
@@ -1,5 +1,4 @@
 import {render, screen} from "@testing-library/react"
-import userEvent from "@testing-library/user-event"
 import AvatarHeading from "./AvatarHeading"
 
 it("Should render the heading", ()=> {
