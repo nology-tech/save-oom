54152503112959adf362e6336c82d0521dd9be0a
n-a-z-a <nadesper@gmail.com>
2022-05-12 16:13:04 +0100

    completed and tested avatar heading

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import Layout from '../../components/Layout/Layout';
 import NavBar from '../../components/navBar/NavBar';
-import AvatarHeading from '../../components/AvatarHeading/AvatarHeading';
+
 
 const Home = () => {
   return (
@@ -8,9 +8,6 @@ const Home = () => {
       <Layout>
         <h1>Save Oom!!</h1>
         <p>Let&apos;s save the little critter!</p>
-        <AvatarHeading avatarHeadingText={"Please add an avatar to save Oom"}/>
-        <AvatarHeading avatarHeadingText={"Name your avatar"}/>
-        <AvatarHeading avatarHeadingText={"Jack's a great name, Oom would be proud!"}/>
       </Layout>
       <NavBar />
     </>
