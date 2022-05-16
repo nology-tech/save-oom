30a69f43f971719318933ad6ee2187073584e2f6
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-05-16 15:46:28 +0100

    Created GamePlay Container

---
+++ src/containers/GamePlay/GamePlay.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const GamePlay = () => {
+  return (
+    <div className='game-play'>
+
+    </div>
+  )
+}
+
+export default GamePlay;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,18 @@
 import Layout from '../../components/Layout/Layout';
 import NavBar from '../../components/navBar/NavBar';
+import TextInput from '../../components/TextInput/TextInput';
+import "./Home.scss"
+
 const Home = () => {
   return (
     <>
       <Layout>
         <h1>Save Oom!!</h1>
         <p>Let&apos;s save the little critter!</p>
+        <TextInput labelText="FIRST NAME" inputType="text" inputName="firstname"/>
+        <TextInput labelText="EMAIL ADDRESS" inputType="email" inputName="email"/>
+        <TextInput labelText="PASSWORD" inputType="password" inputName="password"/>
+        <p className="some-text">This is some TEXT</p>
       </Layout>
       <NavBar />
     </>
