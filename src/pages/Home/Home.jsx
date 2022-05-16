e4db30832b61a5ebc33a905672e9dacd69f6eafb
Patricia Edwards <patriciaje16@gmail.com>
2022-05-16 16:12:57 +0100

    story container

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Layout from '../../components/Layout/Layout';
 import NavBar from '../../components/navBar/NavBar';
+import StoryContainer from '../StoryContainer/StoryContainer';
 
 
 const Home = () => {
   return (
     <>
       <Layout>
+        <StoryContainer />
         <h1>Save Oom!!</h1>
         <p>Let&apos;s save the little critter!</p>
       </Layout>
