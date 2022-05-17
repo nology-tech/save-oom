d9d20985a06bf365237f11050fd4353234a67224
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-05-17 14:43:00 +0100

    Populated phonicsData.js with Level one data

--- src/data/phonicsData.js
+++ src/data/phonicsData.js
@@ -1,10 +1,114 @@
 const phonics = {
     level: "one",
     s: {
-        phonics: "s",
-        text: "as in ‘sat’",
-        soundUrl: ""
-    }
-}
-
-export default phonics;
+      phonics: "s",
+      text: "as in 'sat'",
+      soundUrl: "",
+    },
+    t: {
+      phonics: "t",
+      text: "as in 'tap'",
+      soundUrl: "",
+    },
+    p: {
+      phonics: "p",
+      text: "as in 'pat'",
+      soundUrl: "",
+    },
+    i: {
+      phonics: "i",
+      text: "as in 'ink'",
+      soundUrl: "",
+    },
+    n: {
+      phonics: "n",
+      text: "as in 'nap'",
+      soundUrl: "",
+    },
+    m: {
+      phonics: "m",
+      text: "as in 'mud'",
+      soundUrl: "",
+    },
+    d: {
+      phonics: "d",
+      text: "as in 'dog'",
+      soundUrl: "",
+    },
+    g: {
+      phonics: "g",
+      text: "as in 'gap'",
+      soundUrl: "",
+    },
+    o: {
+      phonics: "s",
+      text: "as in 'on'",
+      soundUrl: "",
+    },
+    c: {
+      phonics: "c",
+      text: "as in 'cat'",
+      soundUrl: "",
+    },
+    k: {
+      phonics: "k",
+      text: "as in 'kit'",
+      soundUrl: "",
+    },
+    ck: {
+      phonics: "ck",
+      text: "as in 'pick'",
+      soundUrl: "",
+    },
+    u: {
+      phonics: "u",
+      text: "as in 'up'",
+      soundUrl: "",
+    },
+    e: {
+      phonics: "e",
+      text: "as in 'egg'",
+      soundUrl: "",
+    },
+    r: {
+      phonics: "r",
+      text: "as in 'rat'",
+      soundUrl: "",
+    },
+    h: {
+      phonics: "h",
+      text: "as in 'hot'",
+      soundUrl: "",
+    },
+    b: {
+      phonics: "b",
+      text: "as in 'big'",
+      soundUrl: "",
+    },
+    f: {
+      phonics: "f",
+      text: "as in 'fox'",
+      soundUrl: "",
+    },
+    ff: {
+      phonics: "ff",
+      text: "as in 'puff'",
+      soundUrl: "",
+    },
+    l: {
+      phonics: "l",
+      text: "as in 'lid'",
+      soundUrl: "",
+    },
+    ll: {
+      phonics: "ll",
+      text: "as in 'bell'",
+      soundUrl: "",
+    },
+    ss: {
+      phonics: "ss",
+      text: "as in 'dress'",
+      soundUrl: "",
+    },
+  };
+  export default phonics;
