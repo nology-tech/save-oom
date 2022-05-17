326f00c53f687ae7f1f94265c588f3d8f9cf0c15
GazalNeb <100412320+GazalNeb@users.noreply.github.com>
2022-05-17 15:17:16 +0100

    level select card, container, with all props (#15)
    * level select card, container, with all props
    
    * removed render of levelSelect page on the screen
    
    * minor change
    
    * updated snapshots
    
    * added props to NavBarHeading in test
    
    * moved NavBar into containers folder

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,14 @@
+import React from 'react';
 import Layout from '../../components/Layout/Layout';
-import NavBar from '../../components/navBar/NavBar';
+import NavBar from '../../containers/navBar/NavBar';
 
 
 const Home = () => {
   return (
     <>
       <Layout>
-        <h1>Save Oom!!</h1>
-        <p>Let&apos;s save the little critter!</p>
+        <NavBar />
       </Layout>
-      <NavBar />
     </>
   );
 };
