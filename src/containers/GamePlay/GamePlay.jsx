a4ddcb2e058d073d87ea17ba27a5adc31a197453
sydney murray <73971994+sydneymurray@users.noreply.github.com>
2022-05-20 14:37:58 +0100

    72 game play container (#16)
    * completed
    
    * Merged with main
    
    * Merged with main
    
    * Created GamePlay Container
    
    * Removed stuff from Home page
    
    * corrected errors
    
    * game-play continer height and width to percentage
    
    Co-authored-by: gazalneb <nebgazal@gmail.com>

---
+++ src/containers/GamePlay/GamePlay.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const GamePlay = () => {
+  return (
+    <section className='game-play'>
+
+    </section>
+  )
+}
+
+export default GamePlay;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
 import NavBar from '../../containers/navBar/NavBar';
+import "./Home.scss";
 
 const Home = () => {
   return (
