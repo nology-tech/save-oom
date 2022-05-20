00fffdeb7afd35cedcd6c87663feed935821008c
AshwiniMadapura <100779874+AshwiniMadapura@users.noreply.github.com>
2022-05-20 15:14:34 +0100

    Created container to display ooms needs (#22)

---
+++ src/containers/OomsNeedsContainer/OomsNeedsContainer.test.jsx
@@ -0,0 +1,10 @@
+
+import { customRender } from '../../utils/testUtils';
+import OomsNeedsContainer from "./OomsNeedsContainer";
+
+
+it('Should render the Ooms need container', () => {
+    const { container } = customRender(<OomsNeedsContainer />);
+    expect(container).toMatchSnapshot();
+});
+
