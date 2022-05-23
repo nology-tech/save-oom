7a4a5893e2ac294f204a4d77fb4bb372f306ae33
Josie Newman <82321832+JosieAilsa@users.noreply.github.com>
2022-05-23 15:33:08 +0100

    added config

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Layout from '../../components/Layout/Layout';
 import NavBar from '../../containers/navBar/NavBar';
 import "./Home.scss";
-
+import {getRoundsForUser} from '../../firebase';
 
 
 const Home = () => {
+  getRoundsForUser("dumbo").then(res => console.log(res)).catch(console.log("error!"))
+
 
   return (
     <>
