b4952a28f52f83f144e243509ec1594e09c45775
Lyssa <ebonwolf00@gmail.com>
2022-05-23 13:21:05 +0100

    testing for animated image component fixed

--- src/components/AnimatedImage/AnimatedImage.test.jsx
+++ src/components/AnimatedImage/AnimatedImage.test.jsx
@@ -1,7 +1,7 @@
 import { customRender } from '../../utils/testUtils';
 import AnimatedImage from '../AnimatedImage/AnimatedImage';
 
-it('Should render the Game Play screen', () => {
+it('Should render the AnimatedImage to screen', () => {
   const { container } = customRender(<AnimatedImage />);
 
   expect(container).toMatchSnapshot();
