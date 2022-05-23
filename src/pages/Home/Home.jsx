28126ff7743870a8d9b0414550ade7f290725ce2
Lyssa <ebonwolf00@gmail.com>
2022-05-23 15:51:24 +0100

    fixed errors with home.jsx still trying to call the game play container

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import GamePlay from '../../containers/GamePlay/GamePlay';
 import Layout from '../../components/Layout/Layout';
 import NavBar from '../../containers/navBar/NavBar';
+import SwingGamePlay from '../SwingGamePlay/SwingGamePlay';
 import './Home.scss';
-
 
 const Home = () => {
   return (
     <>
       <Layout>
         <NavBar />
-        <GamePlay />
+        <SwingGamePlay />
       </Layout>
     </>
   );
