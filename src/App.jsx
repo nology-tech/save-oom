b9df77e88c182e8dbb368500cdab291b3519bb4f
Winzar <connorwinzar@gmail.com>
2022-05-23 16:48:57 +0100

    Initial setup of page, commiting due to need to switch branches

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import './styles/main.scss';
-import 'animate.css';
+import { Route, Routes } from "react-router-dom";
+//import Home from "./pages/Home/Home";
+import AvatarCreation from "./pages/AvatarCreation/AvatarCreation";
+import "./styles/main.scss";
+// import "animate.css";
 
 const App = () => {
   return (
     <Routes>
-      <Route path='*' element={<Home />} />
+      <Route path="*" element={<AvatarCreation />} />
     </Routes>
   );
 };
---
+++ src/pages/AvatarCreation/AvatarCreation.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import NavBar from "../../containers/navBar/NavBar";
+import Layout from "../../components/Layout/Layout";
+import background from "../../assets/images/background.png";
+import AvatarHeading from "../../components/AvatarHeading/AvatarHeading";
+import Button from "../../components/Button/Button";
+
+import "./AvatarCreation.scss";
+
+const AvatarCreation = () => {
+  return (
+    <>
+      <div className="avatarCreation">
+        <Layout>
+          <NavBar className="avatarCreation__navBar" />
+          <img className="avatarCreation__background" src={background} alt="backgroundImage" />
+          <div className="avatarCreation__containerOne">
+            <AvatarHeading avatarHeadingText={"Please add an avatar to save oom"} />
+            <Button buttonStyle={"button-primary"} buttonText="Add Avatar" />
+          </div>
+        </Layout>
+      </div>
+    </>
+  );
+};
+
+export default AvatarCreation;
