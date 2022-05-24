ef80391a6d1de7e624dbee9c373e726afefa7356
Winzar <connorwinzar@gmail.com>
2022-05-24 15:09:47 +0100

    App.jsx correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import LogIn from "./pages/LogIn/LogIn";
-//import LogIn from "./pages/LogIn/LogIn";
-//import Registration from "./pages/Registration/Registration";
-//import Home from './pages/Home/Home';
+import Home from "./pages/Home/Home";
 import "./styles/main.scss";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="*" element={<LogIn />} />
+      <Route path="*" element={<Home />} />
     </Routes>
   );
 };
