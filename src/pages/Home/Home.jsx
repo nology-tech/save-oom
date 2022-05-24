1b4815d78388519dd446332bb663f434e502b98c
n-a-z-a <nadesper@gmail.com>
2022-05-24 14:57:35 +0100

    stats page created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
-import StatsPage from '../StatsPage/StatsPage';
 import NavBar from '../../containers/navBar/NavBar';
 import SwingGamePlay from '../SwingGamePlay/SwingGamePlay';
 import './Home.scss';
@@ -10,13 +9,8 @@ const Home = () => {
   return (
     <>
       <Layout>
-<<<<<<< HEAD
-        <NavBar/>
-        <StatsPage></StatsPage>
-=======
         <NavBar />
         <SwingGamePlay />
->>>>>>> e3e8a086a5b5e9bbc200d76e47ccf14e7dd961b9
       </Layout>
     </>
   );
