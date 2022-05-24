f49ece33dea8f59aed71677697bf2c06ac5ad72d
n-a-z-a <nadesper@gmail.com>
2022-05-24 17:10:12 +0100

    stats page created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
 import NavBar from '../../containers/navBar/NavBar';
-import SwingGamePlay from '../SwingGamePlay/SwingGamePlay';
+// import SwingGamePlay from '../SwingGamePlay/SwingGamePlay';
+import StatsPage from '../../pages/StatsPage/StatsPage'
 import './Home.scss';
 
 const Home = () => {
@@ -9,7 +10,7 @@ const Home = () => {
     <>
       <Layout>
         <NavBar />
-        <SwingGamePlay />
+        <StatsPage/>
       </Layout>
     </>
   );
