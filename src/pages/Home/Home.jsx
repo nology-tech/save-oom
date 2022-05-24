d1b2617225c5a09f9fd034a6f34d4623b8244183
n-a-z-a <nadesper@gmail.com>
2022-05-24 15:13:15 +0100

    stats page created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,10 +3,6 @@ import Layout from '../../components/Layout/Layout';
 import NavBar from '../../containers/navBar/NavBar';
 import SwingGamePlay from '../SwingGamePlay/SwingGamePlay';
 import './Home.scss';
-<<<<<<< HEAD
-
-=======
->>>>>>> e3e8a086a5b5e9bbc200d76e47ccf14e7dd961b9
 
 const Home = () => {
   return (
