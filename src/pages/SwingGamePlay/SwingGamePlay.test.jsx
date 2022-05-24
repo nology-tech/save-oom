f6b5c1f1dc09192688cafdc2a5f99b12ed6e5790
Lyssa <ebonwolf00@gmail.com>
2022-05-24 16:42:38 +0100

    testing error resolved

--- src/pages/SwingGamePlay/SwingGamePlay.test.jsx
+++ src/pages/SwingGamePlay/SwingGamePlay.test.jsx
@@ -1,6 +1,5 @@
 import { customRender } from '../../utils/testUtils';
 import SwingGamePlay from './SwingGamePlay';
-import AnimatedImage from '../../components/AnimatedImage/AnimatedImage';
 
 it('Should render the Swing Game Play screen', () => {
   const { container } = customRender(<SwingGamePlay />);
