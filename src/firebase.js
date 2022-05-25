0333f63f3521a565d2a18a1d1b6706ab74d71176
Josie Newman <82321832+JosieAilsa@users.noreply.github.com>
2022-05-25 09:37:01 +0100

    added functions for right and wrong rounds

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,15 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import {
-    getFirestore,
-    collection,
-    getDocs,
-    doc,
-    getDoc,
-    query,
-    where
-  } from "firebase/firestore";
-  
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  getDoc,
+  query,
+  where,
+  orderBy
+} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -20,7 +20,7 @@ const firebaseConfig = {
   projectId: "save-oom",
   storageBucket: "save-oom.appspot.com",
   messagingSenderId: "874113879773",
-  appId: "1:874113879773:web:863830a75d85fe5b2ed534"
+  appId: "1:874113879773:web:863830a75d85fe5b2ed534",
 };
 
 // Initialize Firebase
@@ -29,74 +29,108 @@ const app = initializeApp(firebaseConfig);
 // Reference the firestore DB
 export const db = getFirestore(app);
 
-
 async function getUsers(db) {
-    const usersCol = collection(db, "users");
-    const usersSnapshot = await getDocs(usersCol);
-    const userList = usersSnapshot.docs.map((doc) => doc.data());
-    return userList;
-  }
-  
-  // gets a User document given a userId.
-  /**
-   *
-   * @param {*} userId
-   * @returns a docSnap (document snapshow), to access use docSnap.data(), returns a Promise.
-   */
-  export async function getUserFromId(userId) {
-    const docRef = doc(db, "users", userId);
-    const docSnap = await getDoc(docRef);
-    return docSnap;
-  }
-  
-  export async function getRoundsForUser(id) {
-    const docRef = doc(db, "users", id);
-    const roundsRef = collection(docRef, "rounds_played");
-    const docSnap = await getDocs(roundsRef);
-    const phonicsArr = docSnap.docs.map((doc) => {
-      return doc.data().phonic;
-    });
-    return phonicsArr;
-  }
+  const usersCol = collection(db, "users");
+  const usersSnapshot = await getDocs(usersCol);
+  const userList = usersSnapshot.docs.map((doc) => doc.data());
+  return userList;
+}
 
-   
-  export async function getSpecificRoundsForUser(id, game) {
-    const q = query(collection(db, "users", id,"rounds_played"), where("gameId", "==", game));
-    const querySnap = await getDocs(q);
-    const phonicsArr = querySnap.docs.map((doc) => {
-      return doc.data().phonic;
-    });
-    return phonicsArr;
-  }
+// gets a User document given a userId.
+/**
+ *
+ * @param {*} userId
+ * @returns a docSnap (document snapshow), to access use docSnap.data(), returns a Promise.
+ */
+export async function getUserFromId(userId) {
+  const docRef = doc(db, "users", userId);
+  const docSnap = await getDoc(docRef);
+  return docSnap;
+}
+export async function getGameRoundsForUser(id, game) {
+  const q = query(
+    collection(db, "users", id, "rounds_played"),
+    where("gameId", "==", game), 
+    orderBy("roundTime", "desc")
+  );
+  const querySnap = await getDocs(q);
+  return querySnap;
+}
 
-     
-  export async function getInCorrectRoundsForUser(id, game) {
-    const q = query(collection(db, "users", id,"rounds_played"), where("gameId", "==", game), where("correct", "==", false));
-    const querySnap = await getDocs(q);
-    const phonicsArr = querySnap.docs.map((doc) => {
-      return doc.data().phonic;
-    });
-    return phonicsArr;
-  }
-       
-  export async function getCorrectRoundsForUser(id, game) {
-    const q = query(collection(db, "users", id,"rounds_played"), where("gameId", "==", game), where("correct", "==", true));
-    const querySnap = await getDocs(q);
-    const phonicsArr = querySnap.docs.map((doc) => {
-      return doc.data().phonic;
-    });
-    return phonicsArr;
-  }
-  
-  
-  
-  
+
+
+export async function getCorrectGameRoundsForUser(id, game) {
+  const q = query(
+    collection(db, "users", id, "rounds_played"),
+    where("gameId", "==", game), 
+    where("correct", "==", true), 
+    orderBy("roundTime", "desc")
+  );
+  const querySnap = await getDocs(q);
+  return querySnap;
+}
+
+
+export async function getIncorrectGameRoundsForUser(id, game) {
+  const q = query(
+    collection(db, "users", id, "rounds_played"),
+    where("gameId", "==", game), 
+    where("correct", "==", false), 
+    orderBy("roundTime", "desc")
+  );
+  const querySnap = await getDocs(q);
+  return querySnap;
+}
+
+
+export async function getArrayOfRounds(id, game, getGameRounds) {
+  const querySnap = await getGameRounds(id, game);
+  const phonicsArr = querySnap.docs.map((doc) => {
+    return doc.data().phonic;
+  });
+  return phonicsArr;
+}
+
+export async function getInCorrectRoundsForUser(id, game) {
+  const q = query(
+    collection(db, "users", id, "rounds_played"),
+    where("gameId", "==", game),
+    where("correct", "==", false)
+  );
+  const querySnap = await getDocs(q);
+  const phonicsArr = querySnap.docs.map((doc) => {
+    return doc.data().phonic;
+  });
+  return phonicsArr;
+}
+
+export async function getCorrectRoundsForUser(id, game) {
+  const q = query(
+    collection(db, "users", id, "rounds_played"),
+    where("gameId", "==", game),
+    where("correct", "==", true)
+  );
+  const querySnap = await getDocs(q);
+  const phonicsArr = querySnap.docs.map((doc) => {
+    return doc.data().phonic;
+  });
+  return phonicsArr;
+}
+
+// export async function getRecentCorrectRoundsForUser(id, game) {
+//   const q = query(collection(db, "users", id,"rounds_played"), where("gameId", "==", game), where("correct", "==", true));
+//   const querySnap = await getDocs(q);
+//   const phonicsArr = querySnap.docs.map((doc) => {
+//     return doc.data().phonic;
+//   });
+//   return phonicsArr;
+// }
+
 //   export async function startNewGame(userId, gameId, level) {
 //     // get the user for the userId
 //     // create a new game entry
 //     // add start date, game id, level and so on
 //     // save as new record.
 //   }
-  
-  export default getUsers;
-  
+
+export default getUsers;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,12 +3,13 @@ import Layout from '../../components/Layout/Layout';
 import NavBar from '../../containers/navBar/NavBar';
 import SwingGamePlay from '../SwingGamePlay/SwingGamePlay';
 import './Home.scss';
-import { getSpecificRoundsForUser,  getInCorrectRoundsForUser } from '../../firebase';
+import { getCorrectGameRoundsForUser,  getIncorrectGameRoundsForUser, getArrayOfRounds} from '../../firebase';
 
 const Home = () => {
 
-  getSpecificRoundsForUser("dumbo", "swing").then(res => console.log(res))
-  getInCorrectRoundsForUser("dumbo", "swing").then(res => console.log(res))
+  // getSpecificRoundsForUser("dumbo", "swing").then(res => console.log(res))
+ getArrayOfRounds("dumbo", "swing", getCorrectGameRoundsForUser).then(res => console.log(res))
+ getArrayOfRounds("dumbo", "swing", getIncorrectGameRoundsForUser).then(res => console.log(res))
 
   return (
     <>
