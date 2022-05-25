098cde8bfc739020a2b771c0192551cef97050cb
n-a-z-a <nadesper@gmail.com>
2022-05-25 14:51:00 +0100

    changes to stats page

--- src/components/Graph/Graph.jsx
+++ src/components/Graph/Graph.jsx
@@ -5,11 +5,16 @@ import './Graph.scss'
 const Graph = () => {
 
     const options = {
+      //horizontal axis variables
         hAxis: {title: "N° OF ATTEMPTS",minValue: 0, maxValue: 6, textStyle:{color:"#FFFFFF"},titleTextStyle:{color:"#FFFFFF"}},
+      //vertical axis variables
         vAxis: {title: "SCORE(%)",minValue: 0, maxValue: 100,textStyle:{color:"#FFFFFF"},titleTextStyle:{color:"#FFFFFF"}},
+      //curved line
         curveType: "function",
+      //size of the points displaying data values
         pointSize:10,
         backgroundColor: "#1e2332",
+      //color of the legend
         legend: {
           textStyle: {
             color: '#FFFFFF',
@@ -20,7 +25,9 @@ const Graph = () => {
   return (
     <div role='graph'>
         <Chart
+        //type of chart
   chartType="LineChart"
+  //data - the first number in each pair is the horizontal axis value - the other is the vertical axis value(score %)
   data={[["N° of Attempts", "Jack"], [1, 40], [2,70 ], [3, 60], [4, 80]]}
   options={options}
   legendToggle
--- src/pages/StatsPage/StatsPage.jsx
+++ src/pages/StatsPage/StatsPage.jsx
@@ -1,5 +1,4 @@
 import './StatsPage.scss'
-// import NavBar from '../../containers/navBar/NavBar'
 import AvatarHeading from '../../components/AvatarHeading/AvatarHeading'
 import Graph from '../../components/Graph/Graph'
 import DropdownButton from '../../components/Dropdown/Dropdown'
@@ -10,20 +9,26 @@ const StatsPage = () => {
 
   return (
     <div className='stats-page'>
-        {/* <NavBar/> */}
-        <div className='stats-page__stats'>
-          {/* <Dropdown LEVEL1={"LEVEL1"} LEVEL2={"LEVEL2"} LEVEL3={"LEVEL3"}/> */}
+
+
+        <section className='stats-page__stats'>
+
+
             <div className='stats-page__top-line'>
             <AvatarHeading avatarHeadingText="Stats for Jack" headingStyle={"dropdown-heading"}className='stats-page__heading'/>
+
             <div className='stats-page__buttons'>
               <DropdownButton option1={"LEVEL 1"} option2={"LEVEL 2"} option3={"LEVEL 3"} option4={"LEVEL 4"} dropdownStyle={"dropdown-level"}/>
               <DropdownButton option1={"JACK"} option2={"HOPE"} option3={"EMMA"} option4={"CONNOR"} dropdownStyle={"dropdown-name"}/>
             </div>
+
             </div>
+
             <div className='stats-page__score-graph'>
                 <h2>Score Over Time</h2>
                 <Graph/>
             </div>
+
             <div className='stats-page__correct-phonemes'>
                 <h2>Correct Phonemes</h2>
                 <ul>
@@ -33,7 +38,8 @@ const StatsPage = () => {
                 </ul>
             </div>
 
-        </div>
+        </section>
+
     </div>
   )
 }
