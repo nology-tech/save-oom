24b7c98f06b3690713d804d817df19ebc54bad4d
Josie Newman <82321832+JosieAilsa@users.noreply.github.com>
2022-05-31 16:32:47 +0100

    editing changes

--- src/pages/Registration/Registration.test.jsx
+++ src/pages/Registration/Registration.test.jsx
@@ -1,15 +1,18 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import Button from "../../components/Button/Button";
 
 import Registration from "./Registration";
 
-it("Should render the Registration screen", () => {
-  render(<Registration />);
+it("Should render the Registration screen", async () => {
+  await act(async () => render(<Registration />));
 
   const paragraph1 = screen.getByText(/This is the registration page/i);
-
   expect(paragraph1).toBeInTheDocument();
-});
+  // await waitFor (() => {
+  //   expect(paragraph1).toBeInTheDocument();
+  // })
+})
 it("Should display the button with the text - Create your account", () => {
   render(<Button buttonText="Create your account" />);
   const paragraph1 = screen.getByText(/Create your account/i);
