772f156ccf83186c2dd18f3b90cf73ae4214f897
Josie Newman <82321832+JosieAilsa@users.noreply.github.com>
2022-05-31 16:37:05 +0100

    rmeoved wait for

--- src/pages/Registration/Registration.test.jsx
+++ src/pages/Registration/Registration.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
 import Button from "../../components/Button/Button";
 
@@ -9,9 +9,6 @@ it("Should render the Registration screen", async () => {
 
   const paragraph1 = screen.getByText(/This is the registration page/i);
   expect(paragraph1).toBeInTheDocument();
-  // await waitFor (() => {
-  //   expect(paragraph1).toBeInTheDocument();
-  // })
 })
 it("Should display the button with the text - Create your account", () => {
   render(<Button buttonText="Create your account" />);
