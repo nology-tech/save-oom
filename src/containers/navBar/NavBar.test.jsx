29e2ea4de329c40324838903d83fed85b04f27e6
Winzar <connorwinzar@gmail.com>
2022-06-01 16:51:26 +0100

    User context test failure fixed

--- src/containers/navBar/NavBar.test.jsx
+++ src/containers/navBar/NavBar.test.jsx
@@ -1,14 +1,29 @@
 import "./NavBar";
 import { customRender } from "../../utils/testUtils";
 import NavBar from "./NavBar";
+import { UserProvider } from "../../contexts/UserContext";
 import { render, screen } from "@testing-library/react";
 
+const userObj = {
+  name: "Jack",
+  id: "UserId1",
+  email: "jack@gamil.com",
+};
+
 it("Should render the navbar", () => {
-  const { container } = customRender(<NavBar />);
+  const { container } = customRender(
+    <UserProvider value={userObj}>
+      <NavBar />
+    </UserProvider>
+  );
   expect(container).toMatchSnapshot();
 });
 it("Should contain the navbar list that contains Level 1 ", () => {
-  render(<NavBar />);
+  render(
+    <UserProvider value={userObj}>
+      <NavBar />
+    </UserProvider>
+  );
   const navItem1 = screen.getByText(/Level 1/i);
   const navItem2 = screen.getByText(/Level 2/i);
   const stats = screen.getByText(/Stats/i);
