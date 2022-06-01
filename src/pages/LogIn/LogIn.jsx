29894be762e1be4c261547d088acf5bdb521949d
n-a-z-a <nadesper@gmail.com>
2022-06-01 14:48:59 +0100

    new commit

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -4,7 +4,6 @@ import Layout from "../../components/Layout/Layout";
 import Logo from "../../components/Logo/Logo";
 import TextInput from "../../components/TextInput/TextInput";
 import "./LogIn.scss";
-<<<<<<< HEAD
 // import PopUp from "../../components/PopUp/PopUp";
 
 const LogIn = () => {
@@ -66,34 +65,13 @@ const LogIn = () => {
             )} */}
             <h1 className="log-in__heading">Welcome!</h1>
             <p className="log-in__top-text">This is the log in page</p>
-<<<<<<< HEAD
             <TextInput className="log-in__input" labelText={"Email"} inputType="email" />
             <TextInput className="log-in__input" labelText={"Password"} inputType="password"/>
-=======
-            <TextInput
-              className="log-in__input"
-              labelText={"Email"}
-              onChangeEvent={(event) => {
-                setLoginEmail(event.target.value);
-              }}
-            />
-            <TextInput
-              className="log-in__input"
-              labelText={"Password"}
-              onChangeEvent={(event) => {
-                setLoginPassword(event.target.value);
-              }}
-            />
->>>>>>> 0b4b1bff148d6c468e65373d0a36e32f94100b0d
             <Button
               className="log-in__button"
               buttonText={"Log in"}
               buttonStyle={"button-secondary"}
-<<<<<<< HEAD
               onClickEvent={loginMessage}
-=======
-              onClickEvent={login}
->>>>>>> 0b4b1bff148d6c468e65373d0a36e32f94100b0d
             />
             <p className="log-in__bottom-text">
               Don&apos;t have an Account?{" "}
