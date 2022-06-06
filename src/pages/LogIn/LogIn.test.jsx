babf43817eda98a5765be8b69711030840a46578
n-a-z-a <nadesper@gmail.com>
2022-06-06 16:39:20 +0100

    updated login testing

--- src/pages/LogIn/LogIn.test.jsx
+++ src/pages/LogIn/LogIn.test.jsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import Button from "../../components/Button/Button";
-import LogIn from "./LogIn";
 
 
 it("Should display the button with the text - log in", () => {
