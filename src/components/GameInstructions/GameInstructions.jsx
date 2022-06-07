b797730fe286f7161526b474b9f16ad1bf23efce
n-a-z-a <nadesper@gmail.com>
2022-06-07 11:09:51 +0100

    game instructions added

--- src/components/GameInstructions/GameInstructions.jsx
+++ src/components/GameInstructions/GameInstructions.jsx
@@ -2,10 +2,28 @@ import React from "react";
 import "./GameInstructions.scss";
 import Button from "../Button/Button";
 
-const GameInstructions = ({ text }) => {
+const GameInstructions = () => {
   return (
     <div className="gameInstructions">
-      <p className="gameInstructions__text">{text}</p>
+      <h1 className="gameInstructions__heading">HOW TO PLAY</h1>
+      <ul className="gameInstructions__text">
+        <li>
+          A phoneme will appear on the screeen and the child will have
+          to say the sound of the phoneme shown
+        </li>
+        <li>
+          If the sound is said correctly, click the green button at the bottom
+          right of the screen
+        </li>
+        <li>
+          If the sound is not said correctly, click the red button at the bottom
+          right of the screen
+        </li>
+        <li>
+          The child will have 1 minute to say as many correct sounds as possible
+        </li>
+        <li>If the child needs help, click the squirrel to hear the sound</li>
+      </ul>
       <Button
         buttonText={"LET'S PLAY"}
         buttonStyle={"button-primary"}
