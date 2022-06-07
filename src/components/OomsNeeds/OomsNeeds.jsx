242b8662005088ac9278a6256a7b3dc3b1f13ecf
Patricia Edwards <patriciaje16@gmail.com>
2022-06-07 15:03:26 +0100

    added functionality to oomsneeds

--- src/components/OomsNeeds/OomsNeeds.jsx
+++ src/components/OomsNeeds/OomsNeeds.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import "./OomsNeeds.scss";
 
 const OomsNeeds = ({ oomsNeed, percentage }) => {
+
+
+
   return (
-    <div className="needs">
-      <img src={oomsNeed} alt="ooms needs" />
+    <>
+      <div className="needs">
+        <div className="needs__image-div">
+          <img className="needs__image" src={oomsNeed} alt="ooms needs" />
+
+        </div>
+
+      </div>
       <p className="needs__percentage">{percentage}%</p>
-    </div>
+
+    </>
   );
 };
 
