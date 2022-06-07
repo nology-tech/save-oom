048e5508aa477f42fa380101e6756345468b26a4
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-06-07 17:07:48 +0100

    Added Dashboard reuseable component

--- src/pages/DashBoard/Dashboard.jsx
+++ src/pages/DashBoard/Dashboard.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
-import NavBarHeading from '../../components/navBarHeading/NavBarHeading'
+//import NavBarHeading from '../../components/navBarHeading/NavBarHeading'
+import AvatarHeading from '../../components/AvatarHeading/AvatarHeading'
+import DashboardItem from '../../components/DashboardItem/DashboardItem'
 import "./Dashboard.scss"
+import smileyImage from  "../../assets/images/smileyImage.svg"
 
 const Dashboard = () => {
   return (
     <div className="dashboard-container">
-        <NavBarHeading headingText="DASHBOARD" headingImg={""} headingStyle={"level-select"}
-          isLocked={false}
-        />
+      <div className="dashboard-container__heading"><AvatarHeading avatarHeadingText={"DASHBOARD"} /></div>
+      <DashboardItem imageSRC="" headingText="Energy Level" contentText="This is some dumm text for explanation "/>
     </div>
   )
 }
 
 export default Dashboard
+
+/*
+<NavBarHeading headingText="DASHBOARD" headingStyle="nav-bar-header__heading"/>
+      <AvatarHeading avatarHeadingText={"Amazing effort Jack"} />
+*/
