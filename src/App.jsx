bb0806666e504a076831e4bcfb8aa9597e2868d9
Hope Sugden <hope_8@hotmail.co.uk>
2022-06-07 16:15:21 +0100

    routing issue fixed for registration page

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,24 @@ import Home from "./pages/Home/Home";
 import LogIn from "./pages/LogIn/LogIn";
 import { useContext } from "react";
 import UserContext from "./contexts/UserContext";
+import Registration from "./pages/Registration/Registration";
 const App = () => {
   const user = useContext(UserContext)
   console.log(user)
   return (
-    <>
-      {!user.user ?
-          <LogIn/> :
-          <Routes>
+    <Routes>
+      {!user.user ? (
+          <>
+          <Route path="/" element={<LogIn />} />
+          </>
+          
+         ): null}
+          <>
+          <Route path="/registration" element={<Registration />} />
           <Route path="*" element={<Home />} />
-        </Routes>}
-    </>
+          </>
+            
+    </Routes>
   )
 };
 
