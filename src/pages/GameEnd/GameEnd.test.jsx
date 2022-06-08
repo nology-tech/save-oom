3e464d39ff041617f518a395a7d6ed59b18dbd93
n-a-z-a <nadesper@gmail.com>
2022-06-08 15:46:22 +0100

    change to test

--- src/pages/GameEnd/GameEnd.test.jsx
+++ src/pages/GameEnd/GameEnd.test.jsx
@@ -2,7 +2,6 @@ import { render, screen } from "@testing-library/react";
 import { customRender } from '../../utils/testUtils';
 import GameEnd from "./GameEnd";
 
-
 it("Should render the Game End screen", () => {
   const { container } = customRender(<GameEnd />);
 
