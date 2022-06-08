b359ba298dca0992f772b90699a54fa7763bf762
n-a-z-a <nadesper@gmail.com>
2022-06-08 15:37:16 +0100

    routing from swing game end added

--- src/pages/GameEnd/GameEnd.jsx
+++ src/pages/GameEnd/GameEnd.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import AvatarHeading from "../../components/AvatarHeading/AvatarHeading";
 import Button from "../../components/Button/Button";
 import "./GameEnd.scss";
 
 const GameEnd = ({score}) => {
+
+  const refreshPage = () => {
+    window.location.reload(false);
+  }
+  
   return (
         <div className="game-end">
             <div className="game-end__top">
                 <p className="game-end__timer">Time&apos;s up</p>
                 <AvatarHeading avatarHeadingText={"Amazing effort Jack"} />
                 <div className="game-end__buttons">
+                  <Link to="/level-select-card-container">
                     <Button buttonStyle={"button-primary"} buttonText={"Back to Home"}/>
-                    <Button buttonStyle={"button-primary"} buttonText={"Play Again"}/>
+                  </Link>
+                 <Button buttonStyle={"button-primary"} buttonText={"Play Again"} onClickEvent={refreshPage}/>
                 </div>
                 <p className="game-end__score">No. of correct sounds: {score}</p>
             </div>
