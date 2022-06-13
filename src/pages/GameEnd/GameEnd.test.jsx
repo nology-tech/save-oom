13fec22c3adcd9790e130e8872dd032179a544d5
n-a-z-a <94549443+n-a-z-a@users.noreply.github.com>
2022-06-13 15:00:05 +0100

    116 routing from swing game end (#56)
    * routing from swing game end added
    
    * test fixed
    
    * change to test
    
    * change to test
    
    * installed shortid

--- src/pages/GameEnd/GameEnd.test.jsx
+++ src/pages/GameEnd/GameEnd.test.jsx
@@ -1,17 +1,10 @@
 import { render, screen } from "@testing-library/react";
-import Button from "../../components/Button/Button";
-
+import { customRender } from '../../utils/testUtils';
 import GameEnd from "./GameEnd";
 
+
 it("Should render the Game End screen", () => {
-  render(<GameEnd />);
+  const { container } = customRender(<GameEnd />);
 
-  const paragraph1 = screen.getByText(/Time's up/i);
-
-  expect(paragraph1).toBeInTheDocument();
-});
-it("Should display the button with the text -Back to Home", () => {
-  render(<Button buttonText="Back to Home" />);
-  const paragraph1 = screen.getByText(/Back to Home/i);
-  expect(paragraph1).toBeInTheDocument();
+  expect(container).toMatchSnapshot();
 });
